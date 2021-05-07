43527d5bb77b3c4451e6935b40216c540dc172bc
ThatHien <domino31298@gmail.com>
2021-05-07 21:51:35 +0700

    create project

---
+++ src/main/java/dulich/com/untils/EncrytedPasswordUtil.java
@@ -0,0 +1,18 @@
+package dulich.com.untils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncrytedPasswordUtil {
+	// Encryte Password with BCryptPasswordEncoder
+    public static String encrytePassword(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+ 
+    public static void main(String[] args) {
+        String password = "1";
+        String encrytedPassword = encrytePassword(password);
+ 
+        System.out.println("Encryted Password: " + encrytedPassword);
+    }
+}
