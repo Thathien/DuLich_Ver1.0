1c5b7c8c3f994756a67cb0fa0bbaf177db089dc3
ThatHien <domino31298@gmail.com>
2021-06-19 17:35:40 +0700

    update temple + log4j

--- src/main/java/dulich/com/controller/user/HomeController.java
+++ src/main/java/dulich/com/controller/user/HomeController.java
@@ -1,13 +1,21 @@
 package dulich.com.controller.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	private static final Logger LOGGER =LogManager.getLogger(HomeController.class);
+	
 	@GetMapping("/")
 	public String index() {
+		if(LOGGER.isInfoEnabled()) {
+			LOGGER.info("HomeController get index");
+		}
+		LOGGER.debug("HomeController get index");
 		return "user/index";
 	}
 }
