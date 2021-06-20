029e2ac07e989f41116394945267aceaab96405f
ThatHien <domino31298@gmail.com>
2021-06-20 12:48:30 +0700

    update security

--- src/main/java/dulich/com/controller/user/HomeController.java
+++ src/main/java/dulich/com/controller/user/HomeController.java
@@ -1,5 +1,7 @@
 package dulich.com.controller.user;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	private static final Logger LOGGER =LogManager.getLogger(HomeController.class);
-	
-	@GetMapping("/")
-	public String index() {
-		if(LOGGER.isInfoEnabled()) {
+	private static final Logger LOGGER = LogManager.getLogger(HomeController.class);
+
+	@GetMapping(value = { "/", "/index" })
+	public String index(HttpServletRequest servletRequest) {
+		if (LOGGER.isInfoEnabled()) {
 			LOGGER.info("HomeController get index");
+//			LOGGER.debug(servletRequest.getRemoteAddr() + " is conecting to HomeController get index");
 		}
-		
-		LOGGER.debug("HomeController get index");
+
 		return "user/index";
 	}
 }
